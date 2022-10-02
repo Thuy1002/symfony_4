082a459d2c8ee494fcfc78b637358b8d0910369b
thuy1002 <thuy1002dangthanh@gmail.com>
2022-10-02 21:28:35 +0700

    first commit

---
+++ src/Controller/AnnimalController.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Animal;
+//use Doctrine\DBAL\Types\TextType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class AnnimalController extends AbstractController
+{
+    /**
+     * @Route("/animal", name="app_annimal")
+     */
+    public function index(): Response
+    {
+//        $animal = $this->getDoctrine()->getRepository(Animal::class)->findAll();
+//        return $this->render('animal/index.html.twig', [
+//            'An' => $animal,
+//        ]);
+        $animal = $this->getDoctrine()->getRepository(Animal::class)->findAll();
+        return $this->render('animal/index.html.twig', array('An' => $animal));
+    }
+  
+    
+    /**
+     * @Route("/annimal/{id}", name="show_animal")
+     *
+     */
+    public function show($id): Response
+    {
+        $animal = $this->getDoctrine()->getRepository(Animal::class)->find($id);
+        return $this->render('animal/show.html.twig', ['detail'=> $animal]);
+    }
+    /**
+     * @Route("/add",name="animal_add")
+     * Method({"GET", "POST"})
+     */
+    public function add( Request $request)
+    {
+        $Animal = new Animal();
+        $form = $this->createFormBuilder($Animal)
+            ->add('name', TextType::class, array('attr' => array('class' => 'form-control')))
+
+
+            ->add('weight', NumberType::class, array('required' => FALSE, 'attr' => array('class' => 'form-control' )))
+            ->add('color', TextareaType::class, array('required' => FALSE, 'attr' => array('class' => 'form-control')))
+            ->add('save', SubmitType::class, array(
+                'label' => 'Create',
+                'attr' => array('class' => 'btn btn-primary mt-3')
+            ))
+            ->getForm();
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $Animal = $form->getData();
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($Animal);
+            $entityManager->flush();
+            return $this->redirectToRoute('app_annimal');
+        }
+        return $this->render('animal/add.html.twig', array('form' => $form->createView()));
+    }
+    /**
+     * @Route("/animal/edit/{id}", name="edit_article")
+     * Method({"GET", "POST"})
+     */
+    public function edit(Request $request, $id) {
+        $animal = new Animal();
+        $animal = $this->getDoctrine()->getRepository(Animal::class)->find($id);
+
+        $form = $this->createFormBuilder($animal)
+            ->add('name', TextType::class, array('attr' => array('class' => 'form-control')))
+            ->add('weight', TextareaType::class, array(
+                'required' => false,
+                'attr' => array('class' => 'form-control')
+            ))
+            ->add('color', TextareaType::class, array(
+                'required' => false,
+                'attr' => array('class' => 'form-control')
+            ))
+            ->add('save', SubmitType::class, array(
+                'label' => 'Update',
+                'attr' => array('class' => 'btn btn-primary mt-3')
+            ))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_annimal');
+        }
+
+        return $this->render('animal/edit.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+    /**
+     * @Route("/animal/delete/{id}")
+     * Method({"DELETE"})
+     */
+    public function delete(Request $request, $id) {
+        $animal = $this->getDoctrine()->getRepository(Animal::class)->find($id);
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($animal);
+        $entityManager->flush();
+        return $this->redirectToRoute('app_annimal');
+
+
+
+    }
+
+
+
+}
---
+++ src/Entity/Animal.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AnimalRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=AnimalRepository::class)
+ */
+class Animal
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $weight;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $color;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getWeight(): ?int
+    {
+        return $this->weight;
+    }
+
+    public function setWeight(?int $weight): self
+    {
+        $this->weight = $weight;
+
+        return $this;
+    }
+
+    public function getColor(): ?string
+    {
+        return $this->color;
+    }
+
+    public function setColor(?string $color): self
+    {
+        $this->color = $color;
+
+        return $this;
+    }
+}
